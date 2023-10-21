3f73ed32c31370f174ad60f95b596eccbd4ddf3a
Kingostyl <ariiqmaazin@gmail.com>
2023-10-21 21:42:20 +0700

    cehckot

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,8 +5,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   port: 3306, //port default 3306 lihat xampp
   username: 'root', // username default xampp root
   password: '', // password default xampp string kosong
-  database: 'belajar_nest_js_kelas_XI',
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  database: 'auth',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: true,
   dropSchema: true,
